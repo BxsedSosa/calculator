24d3d2e21c40f1bcbb98de16f9d3c6a7f62f05c0
BxsedSosa <donovanxabad@gmail.com>
2024-04-20 13:57:21 -0700

    Refactored calculautor by putting most of it through functions to clean up code

--- calculator.py
+++ calculator.py
@@ -5,16 +5,30 @@
 # Print the result to the terminal
 import json
 
+USER_LANG = ["en", "es", "ch", "jp"]
+
 with open("text.json", "r") as file:
-    MESSAGE = json.load(file)
+    MSG = json.load(file)
 
 
 def prompt(key):
-    message = messages(key)
-    print(f"==> {message}")
+    return f"{MSG['arrow']} {key}"
 
 
-def is_valid_number(num):
+def valid_language(text):
+    selected_lang = input(f"{prompt(text)}\n")
+
+    while selected_lang not in USER_LANG:
+        selected_lang = input(f'{prompt(MSG["en"]["not-lang"])}\n')
+
+    return selected_lang
+
+
+def welcome_message(lang):
+    print(prompt(MSG[lang]["welcome"]))
+
+
+def invalid_number(num):
     try:
         int(num)
     except ValueError:
@@ -23,60 +37,56 @@ def is_valid_number(num):
     return False
 
 
-def messages(key):
-    return MESSAGE[selected_lang][key]
+def number_selection(text):
+    user_num = input(prompt(f"{text}\n"))
 
+    while invalid_number(user_num):
+        user_num = input(prompt(f'{MSG[language]["not-num"]}\n'))
+
+    return user_num
+
+
+def operation(text):
+    operator = input(prompt(f"{text}\n"))
+    operator_selection = ["1", "2", "3", "4"]
+
+    while operator not in operator_selection:
+        operator = input(prompt(f'{MSG[language]["not-selection"]}\n'))
+
+    return operator
+
+
+def result(num1, num2, op):
+    match op:
+        case "1":
+            result = float(num1) + float(num2)
+        case "2":
+            result = float(num1) - float(num2)
+        case "3":
+            result = float(num1) * float(num2)
+        case "4":
+            if float(num2) == 0:
+                print(prompt(MSG[language]["zero-division"]))
+                exit(1)
+            result = float(num1) / float(num2)
+    print(prompt(f"The result is: {result}"))
+
+
+
+def main():
+
+    welcome_message(language)
+    number1 = number_selection(MSG[language]["first-num"])
+    number2 = number_selection(MSG[language]["second-num"])
+    operator = operation(MSG[language]["operator"])
+    result(number1, number2, operator)
+
+
+language = valid_language(MSG["en"]["select-lang"])
 
 while True:
-    selected_lang = "en"
+    main()
 
-    prompt("lang")
-    selected_lang = input()
-
-    while selected_lang not in ["en", "es", "ch", "jp"]:
-        selected_lang = "en"
-        prompt("not-lang")
-        selected_lang = input()
-
-    prompt("welcome")
-    prompt("first-num")
-    number1 = input()
-
-    while is_valid_number(number1):
-        prompt("not-num")
-        number1 = input()
-
-    prompt("second-num")
-    number2 = input()
-
-    while is_valid_number(number2):
-        prompt("not-num")
-        number2 = input()
-
-    prompt("operator")
-
-    operation = input()
-
-    while operation not in ["1", "2", "3", "4"]:
-        prompt("not-selection")
-        operation = input()
-
-    match operation:
-        case "1":
-            output = float(number1) + float(number2)
-        case "2":
-            output = float(number1) - float(number2)
-        case "3":
-            output = float(number1) * float(number2)
-        case "4":
-            if float(number2) == 0:
-                prompt("zero-division")
-                exit(1)
-            output = float(number1) // float(number2)
-
-    print("==>", MESSAGE[selected_lang]["result"], output)
-
-    prompt("try-again")
-    resume = input()
-    if resume != "1":
+    resume = input(prompt(f'{MSG[language]['try-again']}\n'))
+    if resume != '1':
         break
